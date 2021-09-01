2d9c84d8809fb6639c384d8aa7733bc18cfd341d
Michael Gabilondo <Michael.Gabilondo@gartner.com>
2021-09-01 14:48:09 -0400

    faiss-gpu

--- mlflow/models/docker_utils.py
+++ mlflow/models/docker_utils.py
@@ -37,6 +37,7 @@ ENV PATH="/miniconda/bin:$PATH"
 ENV JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 ENV GUNICORN_CMD_ARGS="--timeout 60 -k gevent"
 ENV PYTHONPATH="/opt/conda/lib/python3.8/site-packages:/miniconda/lib/python3.8/site-packages"
+RUN conda install -c conda-forge -y faiss-gpu
 # Set up the program in the image
 WORKDIR /opt/mlflow
 
